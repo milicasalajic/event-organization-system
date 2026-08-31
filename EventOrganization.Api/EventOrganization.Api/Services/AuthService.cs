00a80191edf4c606581be1b446707f3f76119753
Milica <milicasalajic13@gmail.com>
2026-08-31 11:25:33 +0200

    done hashing all passwords

--- EventOrganization.Api/EventOrganization.Api/Services/AuthService.cs
+++ EventOrganization.Api/EventOrganization.Api/Services/AuthService.cs
@@ -1,44 +1,52 @@
 using EventOrganization.Api.DTOs.Auth;
 using EventOrganization.Api.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace EventOrganization.Api.Services;
 
 public class AuthService
 {
-	private readonly EventOrganizationDbContext _context;
-	private readonly JwtService _jwtService;
+    private readonly EventOrganizationDbContext _context;
+    private readonly JwtService _jwtService;
+    private readonly PasswordHasher<Korisnik> _passwordHasher;
 
-	public AuthService(
-		EventOrganizationDbContext context,
-		JwtService jwtService)
-	{
-		_context = context;
-		_jwtService = jwtService;
-	}
+    public AuthService(
+        EventOrganizationDbContext context,
+        JwtService jwtService)
+    {
+        _context = context;
+        _jwtService = jwtService;
+        _passwordHasher = new PasswordHasher<Korisnik>();
+    }
 
-	public async Task<LoginResponseDto?> Login(
-		LoginRequestDto request,
-		CancellationToken cancellationToken = default)
-	{
-		var korisnik = await _context.Korisnici
-			.Include(k => k.Uloga) // za ucitavanje povezanog entiteta
-            .Include(k => k.Radnik) //kod radnika trbea ucitati i restoran
+    public async Task<LoginResponseDto?> Login(
+        LoginRequestDto request,
+        CancellationToken cancellationToken = default)
+    {
+        var korisnik = await _context.Korisnici
+            .Include(k => k.Uloga)
+            .Include(k => k.Radnik)
             .FirstOrDefaultAsync(
-				k => k.Email == request.Email,
-				cancellationToken);
+                k => k.Email == request.Email,
+                cancellationToken);
 
-		if (korisnik is null)
-		{
-			return null;
-		}
+        if (korisnik is null)
+        {
+            return null;
+        }
 
-		if (korisnik.Lozinka != request.Lozinka)
-		{
-			return null;
-		}
+        var rezultat = _passwordHasher.VerifyHashedPassword(
+            korisnik,
+            korisnik.Lozinka,
+            request.Lozinka);
 
-		var token = _jwtService.GenerateToken(korisnik);
+        if (rezultat == PasswordVerificationResult.Failed)
+        {
+            return null;
+        }
+
+        var token = _jwtService.GenerateToken(korisnik);
 
         return new LoginResponseDto
         {
